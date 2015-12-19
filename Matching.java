0b19631a2ed24a4de582f486b4872a0e46eedec2
Jeffrey Phung <jjphung@ucsd.edu>
2015-12-19 13:10:59 -0800

    java files

---
+++ Matching.java
@@ -0,0 +1,10 @@
+public class Matching{
+  
+  public void startGame(){
+    new Board();
+  }
+  public static void main (String[] args){
+    Matching newGame = new Matching();
+    newGame.startGame();
+  }
+}
